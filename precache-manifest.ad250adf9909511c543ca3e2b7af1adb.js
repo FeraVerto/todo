5ca5eca9c4fa0b996307173f32da7142d294843d
Клевакин Артем Николаевич <klevakin@tochka.com>
2020-11-24 22:50:36 +0500

    Updates

---
+++ precache-manifest.ad250adf9909511c543ca3e2b7af1adb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0988d723e5e8ee32d44bf901d344c6d6",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "11276ea581ba078488a9",
+    "url": "/todo/static/css/main.8a0d8f79.chunk.css"
+  },
+  {
+    "revision": "1270158010b5545861cd",
+    "url": "/todo/static/js/2.6461233d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/todo/static/js/2.6461233d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11276ea581ba078488a9",
+    "url": "/todo/static/js/main.fa89e569.chunk.js"
+  },
+  {
+    "revision": "e9b3b084cc17457a6264",
+    "url": "/todo/static/js/runtime-main.c58ac565.js"
+  }
+]);
